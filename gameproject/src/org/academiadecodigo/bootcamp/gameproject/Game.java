b454a4add6c06522abaf462de3817f89f5344e98
Daniel Baeta <daniel.baeta@gmail.com>
2017-10-09 19:49:06 +0100

    Teclas F e A implementadas

--- gameproject/src/org/academiadecodigo/bootcamp/gameproject/Game.java
+++ gameproject/src/org/academiadecodigo/bootcamp/gameproject/Game.java
@@ -1,31 +1,39 @@
 package org.academiadecodigo.bootcamp.gameproject;
 
+import org.academiadecodigo.bootcamp.gameproject.blocks.InputBlock;
+import org.academiadecodigo.bootcamp.gameproject.words.InputWord;
+import org.academiadecodigo.simplegraphics.graphics.*;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
 /**
  * Created by Daniel Baeta on 06/10/17.
  */
-public class Game implements KeyboardHandler {
+public class Game {
 
-    public void checkWord(){
+
+
+    public static void main(String[] args) {
+        Rectangle background = new Rectangle(10, 10, 700, 900);
+        background.fill();
+        InputBlock box = new InputBlock();
+        InputWord word = new InputWord();
+
+    }
+    public void checkWord() {
 
     }
 
-    public void start(){
+    public void start() {
     }
 
-    public void countLives(){
-
-    }
-
-    @Override
-    public void keyPressed(KeyboardEvent keyboardEvent) {
+    public void countLives() {
 
     }
 
-    @Override
-    public void keyReleased(KeyboardEvent keyboardEvent) {
 
-    }
+
 }
--- gameproject/src/org/academiadecodigo/bootcamp/gameproject/words/InputWord.java
+++ gameproject/src/org/academiadecodigo/bootcamp/gameproject/words/InputWord.java
@@ -1,11 +1,48 @@
 package org.academiadecodigo.bootcamp.gameproject.words;
 
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
 /**
  * Created by Daniel Baeta on 06/10/17.
  */
-public class InputWord extends Word {
+public class InputWord extends Word implements KeyboardHandler {
 
-    public void getInput(){
+    Word inputText = new Word();
+    String word = "";
+
+    public InputWord(){
+        inputText.grow(30, 20);
+        inputText.draw();
+        inputKey();
+    }
+
+    public void inputKey(){
+        Keyboard keyboard = new Keyboard(this);
+        KeyboardEvent keyboardEvent = new KeyboardEvent();
+        keyboardEvent.setKey(KeyboardEvent.KEY_F);
+        keyboardEvent.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(keyboardEvent);
+
+        KeyboardEvent keyboardEvent1 = new KeyboardEvent();
+        keyboardEvent1.setKey(KeyboardEvent.KEY_A);
+        keyboardEvent1.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(keyboardEvent1);
+        
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        word += ((char) keyboardEvent.getKey());
+        inputText.setText(word);
+        inputText.show();
+
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
 
     }
 }
